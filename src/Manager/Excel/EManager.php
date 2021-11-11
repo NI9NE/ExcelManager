6b18ca6103618794a5cf715ee57a8213e4a9e510
ni9ne <ni9ne@outlook.com>
2021-11-11 22:13:27 +0800

    'test'

---
+++ src/Manager/Excel/EManager.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Manager\Excel;
+
+class EManager{
+
+    public function __construct()
+    {
+
+    }
+
+    public static function Hello()
+    {
+        echo "hello composer";
+    }
+
+}
